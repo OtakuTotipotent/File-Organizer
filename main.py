bee53e7e504ca184dc53bf23caf32095cd9d4b47
Afnan Muhammad <otakutotipotent@gmail.com>
2025-11-20 23:21:03 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+if os.path.exists("Day3.txt"):
+    os.makedirs("Completed", exist_ok=True)
+    shutil.move("Day3.txt", "Completed")
+else:
+    print("Target file doesn't exist!")
