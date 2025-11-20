59c7fa35772cf8f6037e9fb89069bb8b74ad9637
Afnan Muhammad <otakutotipotent@gmail.com>
2025-11-20 23:27:45 +0500

    Initial commit 2nd

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import shutil
 if os.path.exists("Day3.txt"):
     os.makedirs("Completed", exist_ok=True)
     shutil.move("Day3.txt", "Completed")
+    print("File moved successfully!")
 else:
     print("Target file doesn't exist!")
